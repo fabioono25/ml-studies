325e59fc01051cf838788c7f53dec9d31251597c
fabioono25 <fabioono25@gmail.com>
2024-02-17 19:06:31 +0100

    data preprocessing - R

--- 1-Data Preprocessing/categorical_data.R
+++ 1-Data Preprocessing/categorical_data.R
@@ -1,2 +1,37 @@
-# print some message using R language
-print("First test using R in VSCode.")
+# Part 1: importing the dataset
+setwd("~/study/ml-studies/1-Data Preprocessing")
+dataset <- read.csv("Data.csv")
+# dataset = dataset[1:3]  # only the first 3 columns
+# print(dataset)
+
+# Part 2: Taking care of missing data (sometimes not)
+dataset$Age <- ifelse(is.na(dataset$Age), ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)), dataset$Age) # nolint
+# na.rm meaning: remove NA values from the dataset
+dataset$Salary <- ifelse(is.na(dataset$Salary), ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)), dataset$Salary)
+
+# Part 3: Encoding categorical data (Country and Purchased) (sometimes not)
+
+# using the factor function to encode the categorical data
+dataset$Country <- factor(dataset$Country, levels = c("France", "Spain", "Germany"), labels = c(1, 2, 3))
+
+dataset$Purchased <- factor(dataset$Purchased, levels = c("No", "Yes"), labels = c(0, 1))
+
+
+# Part 4: Splitting the dataset into the Training set and Test set
+
+# install.packages('caTools')
+library(caTools)
+set.seed(123) # to make the result reproducible
+split <- sample.split(dataset$Purchased, SplitRatio = 0.8) # 80% of the data will be used for training
+# split: TRUE goes to training set, FALSE goes to test set
+
+# create the training and test sets
+training_set <- subset(dataset, split == TRUE)
+test_set <- subset(dataset, split == FALSE)
+
+# Part 5: Feature Scaling (turn the values into the same scale)
+# example: age and salary are not in the same scale. Age is from 27 to 50, while salary is from 48000 to 83000
+# Euclidian distance will be dominated by the salary, so we need to scale the values
+
+training_set[, 2:3] <- scale(training_set[, 2:3])
+test_set[, 2:3] <- scale(test_set[, 2:3])
--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 ## Studies about Machine Learning
 
-- Data Preprocessing in Python
-- 
+### Data Preprocessing
+
+Data preprocessing is a crucial step in the machine learning pipeline that involves **cleaning**, **transforming**, and **organizing raw data** into a format suitable for training and evaluating machine learning models. The quality of the input data significantly impacts the performance and generalization ability of the model. Here are some key aspects of data preprocessing:
+
+* Data Cleaning: how to handle missing values and outliers.
+* Data Transformation: feature scaling (standardize/normalize numerical features), one-hot encoding (convert categorical values into binary matrix).
+* Data Reduction: techniques to retain essential information, sampling.
+* Data Organization: splitting the database (training and validation), time-based splitting.
+
+Effective data preprocessing is essential for building robust and accurate machine learning models. It requires a good understanding of the specific characteristics of the dataset and the requirements of the
+chosen machine learning algorithm. Iterative refinement of the preprocessing steps is often necessary to achieve optimal results.
+
+### Regression
